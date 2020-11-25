73d06206ccd149ede455da0df8616ee614a63cbc
vuongvgc <vuongvgc@gmail.com>
2020-11-25 18:05:18 +0700

    create form StreamCreate: pass component and text connect with redux vr 1

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 class StreamCreate  extends React.Component {
+    renderInput(propsForm) {
+        console.log(propsForm)
+        return <input onChange={propsForm.input.onChange} value={propsForm.input.value} />
+    }
     render(){
-        console.log(this.props)
+        // console.log(this.props)
         return(
             <div>
-                <Field name="title" component="input" type="text"/>
-                <Field name="description" component="input" type="textArea" />
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
             </div>
         )
     }
