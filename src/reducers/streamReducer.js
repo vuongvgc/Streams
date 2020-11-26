ed00c252de20b927964c6839c326a6c4f824cada
vuongvgc <vuongvgc@gmail.com>
2020-11-26 22:17:04 +0700

    create stream reducer: fetch_stream, edit_stream, create_stream

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,23 @@
+import { 
+    SIGN_IN,
+    SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM ,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+const streamReducer = (state={}, action) => {
+    const {payload, type } = action.payload
+    switch(type) {
+        case FETCH_STREAM:
+            return {...state, [payload.id]: payload};
+        case EDIT_STREAM:
+            return {...state, [payload.id]: payload};
+        case CREATE_STREAM:
+            return {...state,  [payload.id]: payload}
+        default:
+            return state
+    }
+}
+export default  streamReducer;
