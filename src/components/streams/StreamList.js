c4b23c71a5ba8fa1a7f685a1a49dde4d47cef52d
vuongvgc <vuongvgc@gmail.com>
2020-11-28 09:07:19 +0700

    render StreamList

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -5,9 +5,28 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+  renderStream() {
+    return this.props.streams.map((item) => {
+      const { id, title, description } = item;
+      return (
+        <div className="alert alert-dark" role="alert" key={id}>
+          <h4 className="text-danger">{title}</h4>
+          <p>{description}</p>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>fetchStreams</div>;
+    console.log(this.props.streams);
+    return (
+      <div className="container">
+        <h2 className="text-center text-uppercase">Streams</h2>
+        {this.renderStream()}
+      </div>
+    );
   }
 }
-
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
+};
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
