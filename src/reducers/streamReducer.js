e89e897826332df9e886eb59fa79cbd79efe7040
vuongvgc <vuongvgc@gmail.com>
2020-11-28 08:41:34 +0700

    create stream reducer: create store streams

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,26 +1,26 @@
-import { 
-    SIGN_IN,
-    SIGN_OUT, 
-    CREATE_STREAM,
-    FETCH_STREAMS,
-    FETCH_STREAM ,
-    EDIT_STREAM,
-    DELETE_STREAM
-} from '../actions/types';
-import _ from 'lodash';
-const streamReducer = (state={}, action) => {
-    const {payload, type } = action.payload
-    switch(type) {
-        case FETCH_STREAM:
-            return {...state, [payload.id]: payload};
-        case EDIT_STREAM:
-            return {...state, [payload.id]: payload};
-        case CREATE_STREAM:
-            return {...state,  [payload.id]: payload};
-        case DELETE_STREAM:
-            return _.omit(state, payload.id);
-        default:
-            return state
-    }
-}
-export default  streamReducer;
+import {
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "../actions/types";
+import _ from "lodash";
+const streamReducer = (state = {}, action) => {
+  const { payload, type } = action;
+  switch (type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(payload, "id") };
+    case FETCH_STREAM:
+      return { ...state, [payload.id]: payload };
+    case EDIT_STREAM:
+      return { ...state, [payload.id]: payload };
+    case CREATE_STREAM:
+      return { ...state, [payload.id]: payload };
+    case DELETE_STREAM:
+      return _.omit(state, payload.id);
+    default:
+      return state;
+  }
+};
+export default streamReducer;
