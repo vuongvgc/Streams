0bb63d94b858094e7a01fad0e8a01eec7fe8b14e
vuongvgc <vuongvgc@gmail.com>
2020-12-01 05:43:41 +0700

    pull code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,70 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "./types";
+import streams from "../apis/streams";
 export const signIn = (userID) => {
-    return {
-        type: SIGN_IN,
-        payload: userID
-    }
-}
+  return {
+    type: SIGN_IN,
+    payload: userID,
+  };
+};
 export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    }
-}
+  return {
+    type: SIGN_OUT,
+  };
+};
+export const createStream = (formValue) => {
+  return async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post("/streams", {
+      ...formValue,
+      userId,
+    });
+    dispatch({
+      type: CREATE_STREAM,
+      payload: response.data,
+    });
+  };
+};
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get("/streams");
+    dispatch({
+      type: FETCH_STREAMS,
+      payload: response.data,
+    });
+  };
+};
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({
+      type: FETCH_STREAM,
+      payload: response.data,
+    });
+  };
+};
+export const editStream = (id, formValues) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({
+      type: EDIT_STREAM,
+      payload: response.data,
+    });
+  };
+};
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({
+      type: DELETE_STREAM,
+      payload: id,
+    });
+  };
+};
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,9 +1,65 @@
-import React from 'react';
-const StreamList  = () => {
-    return(
-        <div>
-            StreamList
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchStreams } from "../../actions";
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  renderAdmin = (stream) => {
+    if (stream.userId === this.props.currentId) {
+      return (
+        <div className="row">
+          <button className="btn btn-primary mx-1">Edit</button>
+          <button className="btn btn-danger">Delete</button>
         </div>
-    )
+      );
+    }
+  };
+  renderCreateStream = () => {
+    if (this.props.isSignedIn) {
+      return (
+        <div className="text-right">
+          <Link to="/streams/new" className="btn btn-primary">
+            Create Streams
+          </Link>
+        </div>
+      );
+    }
+  };
+  renderStream() {
+    return this.props.streams.map((item) => {
+      const { id, title, description } = item;
+      return (
+        <div className="alert alert-dark row" role="alert" key={id}>
+          <div className="col-2">
+            <i className="fa fa-video W-100"></i>
+          </div>
+          <div className="col-6">
+            <h4 className="text-danger">{title}</h4>
+            <p>{description}</p>
+          </div>
+          <div className="col-4">{this.renderAdmin(item)}</div>
+        </div>
+      );
+    });
+  }
+  render() {
+    console.log(this.props.streams);
+    return (
+      <div className="container">
+        <h2 className="text-center text-uppercase">Streams</h2>
+        {this.renderStream()}
+        {this.renderCreateStream()}
+      </div>
+    );
+  }
 }
-export default StreamList;
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams),
+    currentId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
+};
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
