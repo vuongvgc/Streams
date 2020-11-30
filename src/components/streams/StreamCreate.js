3820b49af3507dfdd85cc756b71c9329923f430b
vuongvgc <vuongvgc@gmail.com>
2020-11-30 22:55:53 +0700

    fit reducer auth

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,70 +1,61 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
-import { createStream } from "../../actions/index";
+import { createStream } from "../../actions";
 class StreamCreate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: " ",
-      description: " ",
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
+  renderError({ error, touched }) {
+    if (error && touched) {
+      return <div className="alert alert-danger">{error}</div>;
+    }
   }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const name = target.name;
-    // console.log(name);
-    this.setState({
-      [name]: target.value,
-    });
-  }
-  handleSubmit = (event) => {
-    const stream = {
-      title: this.state.title,
-      description: this.state.description,
-    };
-    this.props.createStream(stream);
-    event.preventDefault();
+  renderInput = ({ input, label, meta }) => {
+    // console.log(propsForm)
+    const className = meta.error && meta.touched ? "text-danger" : "";
+    return (
+      <div className="form-group">
+        <label className={className}>{label}</label>
+        <input className="form-control" {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+  onSubmit = (formValue) => {
+    // console.log(formValue);
+    // console.log(this.props.createStream());
+    this.props.createStream(formValue);
   };
   render() {
-    const { title, description } = this.state;
+    // console.log(this.props)
     return (
-      <form>
-        <div className="form-group">
-          <label>
-            Title:
-            <input
-              name="title"
-              type="text"
-              value={title}
-              onChange={this.handleInputChange}
-              className="form-control"
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label>
-            Description
-            <input
-              name="description"
-              type="text"
-              value={description}
-              onChange={this.handleInputChange}
-              className="form-control"
-            />
-          </label>
-        </div>
-        <button
-          className="btn btn-success"
-          type="button"
-          onClick={this.handleSubmit}
-        >
-          Submit
-        </button>
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field
+          name="title"
+          component={this.renderInput}
+          label="Enter a title"
+        />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter a description"
+        />
+        <button className="btn btn-primary">Submit</button>
       </form>
     );
   }
 }
-export default connect(null, { createStream: createStream })(StreamCreate);
+const validate = (formValues) => {
+  let error = {};
+  if (!formValues.title) {
+    error.title = "You must enter title";
+  }
+  if (!formValues.description) {
+    error.description = "You must enter description";
+  }
+  return error;
+};
+const warpForm = reduxForm({
+  form: "streamCreate",
+  validate: validate,
+})(StreamCreate);
+
+export default connect(null, { createStream: createStream })(warpForm);
