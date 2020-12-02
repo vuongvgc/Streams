0365b9b5245c61ee01b7aafe1d74308f743b7cd5
vuongvgc <vuongvgc@gmail.com>
2020-12-02 22:09:21 +0700

    deleteStream: use StopPropagation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 const Modal = (props) => {
   return ReactDOM.createPortal(
     <div
@@ -7,35 +8,32 @@ const Modal = (props) => {
       id="exampleModal"
       tabindex="-1"
       role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
+      onClick={() => history.push("/")}
     >
-      <div class="modal-dialog" role="document">
+      <div
+        class="modal-dialog"
+        role="document"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">
-              Modal title
+              Delete Stream
             </h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
           </div>
-          <div class="modal-body">...</div>
+          <div class="modal-body">
+            Are you sure want to delete this stream ?
+          </div>
           <div class="modal-footer">
+            <button type="button" class="btn btn-primary">
+              Delete
+            </button>
             <button
               type="button"
               class="btn btn-secondary"
               data-dismiss="modal"
             >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">
-              Save changes
+              Cancel
             </button>
           </div>
         </div>
